c7885f560bc5d3e86c140470c1baf24b87143b9e
ramonmontenegropng <ramonmontenegro.png@gmail.com>
2024-04-06 19:00:52 -0300

    fix: correction of some printing errors in desafio 03

--- desafioExtra03/src/desafioExtra03/Main.java
+++ desafioExtra03/src/desafioExtra03/Main.java
@@ -6,17 +6,22 @@ public class Main {
         Funcionario funcionario1 = new Funcionario("Ramon", 1000, 0.12);
         Funcionario funcionario2 = new Funcionario("Arthur", 15000, 0.1);
         
+        System.out.println("GERENCIADOR DE FUNCIONÁRIOS");
+        
         System.out.println("Detalhes dos funcionários antes do aumento:");
+        
         System.out.println("Funcionário 1 é " + funcionario1);
         System.out.println("Funcionário 2 é " + funcionario2);
 
-        funcionario1.aumentoSalario(10);
+        funcionario1.aumentoSalario(7);
 
         System.out.println("Detalhes dos funcionários após o aumento:");
+        
         System.out.println("Funcionário 1 é " + funcionario1);
         System.out.println("Funcionário 2 é " + funcionario2);
 
         System.out.println("Salários líquidos dos funcionários:");
+        
         System.out.println("Funcionário 1 é " + funcionario1.obterSalarioLiquido());
         System.out.println("Funcionário 2 é " + funcionario2.obterSalarioLiquido());
     }
